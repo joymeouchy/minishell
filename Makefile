4de88306f1f9489d9f8275b227579f55a93d91ab
Joy Meouchy <jmeouchy@c1r3s6.42beirut.com>
2025-01-14 09:45:32 +0200

    refactoring in parser

--- Makefile
+++ Makefile
@@ -26,9 +26,11 @@ $(LIBFT):
 
 clean:
 		rm -f $(OBJ)
+		make -C ./libft clean
 
 fclean: clean
 		rm -f $(NAME)
+		make -C ./libft fclean
 
 re: fclean all
 .PHONY: all clean fclean re
--- src/parser/parser.c
+++ src/parser/parser.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lkhoury <lkhoury@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jmeouchy <jmeouchy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 11:42:06 by lkhoury           #+#    #+#             */
-/*   Updated: 2025/01/13 16:40:28 by lkhoury          ###   ########.fr       */
+/*   Updated: 2025/01/14 09:42:55 by jmeouchy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,30 +20,26 @@ char	*command_line_input()
 	return (input);
 }
 
+
+void one_element_input_to_list(char *input, t_list *list, int *start, int *i)
+{
+	insert_at_end_list(list, ft_substr(input, *start, *i - *start));
+	insert_at_end_list(list, ft_substr(input, *start, 1));
+	*start = *i + 1;
+}
+void two_element_input_to_list(char *input, t_list *list, int *start, int *i)
+{
+	insert_at_end_list(list, ft_substr(input, *start, *i - *start));
+	insert_at_end_list(list, ft_substr(input, *start, 2));
+	*start = ++*i + 1;
+}
+
 int split_redirections(char *input, t_list *list, int start, int *i)
 {
-	if (input[*i] == '<' && input[*i + 1] == '<')
-	{
-		insert_at_end_list(list, ft_substr(input, start, *i - start));
-		start = *i;
-		insert_at_end_list(list, ft_substr(input, start, *i - start + 2));
-		start = ++*i + 1;
-	}
-	else if (input[*i] == '>' && input[*i + 1] == '>')
-	{
-		insert_at_end_list(list, ft_substr(input, start, *i - start));
-		start = *i;
-		insert_at_end_list(list, ft_substr(input, start, *i - start + 2));
-		start = ++*i + 1;
-	}
+	if ((input[*i] == '<' && input[*i + 1] == '<') ||(input[*i] == '>' && input[*i + 1] == '>'))
+			two_element_input_to_list(input, list, &start, i);
 	else if (input[*i] == '<' || input[*i] == '>')
-	{
-		printf("hiii");
-		insert_at_end_list(list, ft_substr(input, start, *i - start));
-		start = *i;
-		insert_at_end_list(list, ft_substr(input, start, *i - start + 1));
-		start = *i + 1;
-	}
+		one_element_input_to_list(input, list, &start, i);	
 	return (start);
 }
 
@@ -53,8 +49,7 @@ int split_symbols(char *input, t_list *list, int start, int *i)
 	if (input[*i] == '|')
 	{
 		insert_at_end_list(list, ft_substr(input, start, *i - start));
-		start = *i;
-		insert_at_end_list(list, ft_substr(input, start, *i - start + 1));
+		insert_at_end_list(list, ft_substr(input, start, 1));
 		start = *i + 1;
 	}
 	else if (input[*i] == ' ') 
@@ -63,8 +58,17 @@ int split_symbols(char *input, t_list *list, int start, int *i)
 		while (input[*i] == ' ')
 			(*i)++;
 		start = *i;
+		// insert_at_end_list(list, ft_substr(input, start, *i - start));
 	}
 	return (start);
+}
+
+void quotes_to_node(char *input, t_list *list, int start, int *i)
+{
+	while(input[*i] != 22 && input[*i] != 27)
+		i++;
+	insert_at_end_list(list, ft_substr(input, start, *i - start));
+	start = *i + 1;
 }
 
 t_list	*input_to_list(char *input)
@@ -78,8 +82,10 @@ t_list	*input_to_list(char *input)
 	start = i;
 	while (input[i])
 	{
-		start = split_redirections(input, list, start, &i);
+		if (input[i] == 22 || input[i] == 27)
+			quotes_to_node(input, list, start, &i);
 		start = split_symbols(input, list, start, &i);
+		start = split_redirections(input, list, start, &i);
 		i++;
 		if (!input[i])
 			insert_at_end_list(list, ft_substr(input, start, i - start));
