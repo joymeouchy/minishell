d6945f5a34ed1b65e6fc5dfc87945d657f4459da
Joy Meouchy <joymeouchy@gmail.com>
2025-04-26 12:11:14 +0300

    tree correct, commands in wrong order

--- src/parser/shunting_yard.c
+++ src/parser/shunting_yard.c
@@ -6,7 +6,7 @@
 /*   By: jmeouchy <jmeouchy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 12:39:06 by lkhoury           #+#    #+#             */
-/*   Updated: 2025/04/24 11:38:01 by jmeouchy         ###   ########.fr       */
+/*   Updated: 2025/04/26 12:03:53 by jmeouchy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,39 +24,28 @@ void	push_stack_to_other(t_stack *src, t_stack *dst)
 t_stack	*shunting_yard(t_list *list)
 {
 	t_stack		*stack;
-	t_stack		*cmd_stack;
-	t_stack		*pipe_stack;
-	t_list_node	*temp;
-
-	temp = list->head;
-	stack = malloc(sizeof(t_stack));
-	cmd_stack = malloc(sizeof(t_stack));
-	pipe_stack = malloc(sizeof(t_stack));
-	init_stack(list, stack);
-	init_stack(list, cmd_stack);
-	init_stack(list, pipe_stack);
-	while (temp)
-	{
-		if (temp->token > 6)
-			push(temp->data, temp->token, stack);
-		else
-		{
-			if (pipe_stack->stack[pipe_stack->top].token == 0)
-				push_stack_to_other(cmd_stack, stack);
-			if (pipe_stack->top > 0)
-				push_stack_to_other(pipe_stack, stack);
-			if(temp->token == 0)
-				push(temp->data, temp->token, pipe_stack);
-			else
-			push(temp->data, temp->token, cmd_stack);
-		}
-		temp = temp->next;
-	}
-	if (cmd_stack->top > -1)
-		push_stack_to_other(cmd_stack, stack);
-	if (pipe_stack->top > -1)
-		push_stack_to_other(pipe_stack, stack);
-	free_stack(cmd_stack);
-	free_stack(pipe_stack);
-	return (stack);
+ 	t_stack		*cmd_stack;
+ 	t_list_node	*temp;
+ 
+ 	temp = list->head;
+ 	stack = malloc(sizeof(t_stack));
+ 	cmd_stack = malloc(sizeof(t_stack));
+ 	init_stack(list, stack);
+ 	init_stack(list, cmd_stack);
+ 	while (temp)
+ 	{
+ 		if (temp->token > 0)
+ 			push(temp->data, temp->token, stack);
+ 		else
+ 		{
+ 			if (cmd_stack->stack[cmd_stack->top].token == 0)
+ 				push_stack_to_other(cmd_stack, stack);
+ 			push(temp->data, temp->token, cmd_stack);
+ 		}
+ 		temp = temp->next;
+ 	}
+ 	if (cmd_stack->top > -1)
+ 		push_stack_to_other(cmd_stack, stack);
+ 	free_stack(cmd_stack);
+ 	return (stack);
 }
--- src/structures/tree_utils.c
+++ src/structures/tree_utils.c
@@ -3,120 +3,121 @@
 /*                                                        :::      ::::::::   */
 /*   tree_utils.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmeouchy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: jmeouchy <jmeouchy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/13 06:42:48 by jmeouchy          #+#    #+#             */
-/*   Updated: 2025/04/24 18:37:08 by jmeouchy         ###   ########.fr       */
+/*   Created: 2025/04/26 12:05:32 by jmeouchy          #+#    #+#             */
+/*   Updated: 2025/04/26 12:10:00 by jmeouchy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../includes/minishell.h"
-
-t_tree	*init_tree(void)
-{
-	t_tree	*tree;
-
-	tree = malloc(sizeof(t_tree));
-	tree->root = NULL;
-	return (tree);
-}
-
-void	print_inorder(t_tree_node *node)
-{
-	if (node == NULL)
-		return ;
-	print_inorder(node->left);
-	printf("Data: %s, Token: %d\n", node->data, node->token);
-	print_inorder(node->right);
-}
-
-t_tree_node	*create_tree_node(char *data, e_tokens token, bool *flag_inserted_node)
-{
-	t_tree_node	*new_node;
-
-	new_node = (t_tree_node *)malloc(sizeof(t_tree_node));
-	new_node->data = data;
-	new_node->token = token;
-	new_node->left = NULL;
-	new_node->right = NULL;
-	*flag_inserted_node = true;
-	return (new_node);
-}
-
-bool check_pipe_precendence(e_tokens current_token, t_tree_node *node)
-{
-	if (node == NULL)
-		return (true);
-	if (current_token == PIPE && current_token < node->token)
-		return (false);
-	else
-		return (true);
-	
-}
-
-
-bool check_builtin_cmd(t_tree_node *node)
-{
-	if (node == NULL)
-		return (true);
-	if (node->token == BUILT_IN || node->token == COMMAND)
-		return (false);
-	else
-		return (true);
-}
-
-bool check_word_cmd_branches(t_tree_node *node, t_tree_node *node_direction)
-{
-	if (node == NULL)
-		return (true);
-	// if ((node->token == WORD || node->token == COMMAND || node->token == BUILT_IN)
-	// 	&& (current_token == COMMAND || current_token == BUILT_IN))
-	// 	return (false);
-	if ((node->token == WORD || node->token == COMMAND || node->token == BUILT_IN) && node_direction != NULL)
-		return (true);
-	else
-		return (true);
-}
-
-t_tree_node *insert(t_tree_node *node, char *data, e_tokens token, bool *flag_inserted_node)
-{
-    if (node == NULL)
-	{
-        return (create_tree_node(data, token, flag_inserted_node));
-	}
-	if (*flag_inserted_node == false
-		&& check_pipe_precendence(token, node->right)
-		&& check_word_cmd_branches(node, node->left)
-		)
-	{
-		node->right = insert(node->right, data, token, flag_inserted_node);
-	}
-	if (*flag_inserted_node == false
-		&& check_pipe_precendence(token, node->left)
-		&& check_word_cmd_branches(node, node->right)
-	)
-	{
-		node->left = insert(node->left, data, token, flag_inserted_node);
-	}
-    return (node);
-}
-
-t_tree	*stack_to_tree(t_stack *stack)
-{
-	t_tree	*tree;
-	bool	flag_inserted_node;
-
-	tree = init_tree();
-	flag_inserted_node = false;
-	tree->root = create_tree_node(stack->stack[stack->top].data,
-			stack->stack[stack->top].token, &flag_inserted_node);
-	stack->top--;
-	while (stack->top > -1)
-	{
-		flag_inserted_node = false;
-		tree->root = insert(tree->root, stack->stack[stack->top].data,
-				stack->stack[stack->top].token, &flag_inserted_node);
-		stack->top--;
-	}
-	return (tree);
-}
+ #include "../../includes/minishell.h"
+ 
+ t_tree	*init_tree(void)
+ {
+ 	t_tree	*tree;
+ 
+ 	tree = malloc(sizeof(t_tree));
+ 	tree->root = NULL;
+ 	return (tree);
+ }
+ 
+ void	print_inorder(t_tree_node *node)
+ {
+ 	if (node == NULL)
+ 		return ;
+ 	printf("Data: %s, Token: %d\n", node->data, node->token);
+	if (node->right)
+		printf("right:%s\n", node->right->data);
+	if(node->left)
+		printf("left:%s\n", node->left->data);
+ 	print_inorder(node->left);
+ 	print_inorder(node->right);
+ }
+ 
+ t_tree_node	*create_tree_node(char *data, e_tokens token, bool *flag_inserted_node)
+ {
+ 	t_tree_node	*new_node;
+ 
+ 	new_node = (t_tree_node *)malloc(sizeof(t_tree_node));
+ 	new_node->data = data;
+ 	new_node->token = token;
+ 	new_node->left = NULL;
+ 	new_node->right = NULL;
+ 	*flag_inserted_node = true;
+ 	return (new_node);
+ }
+ 
+ bool check_pipe_precendence(e_tokens current_token, t_tree_node *node)
+ {
+ 	if (node == NULL)
+ 		return (true);
+ 	if (current_token == PIPE && current_token < node->token)
+ 		return (false);
+ 	else
+ 		return (true);
+ 	
+ }
+ 
+ 
+ bool check_builtin_cmd(t_tree_node *node)
+ {
+ 	if (node == NULL)
+ 		return (true);
+ 	if (node->token == BUILT_IN || node->token == COMMAND)
+ 		return (false);
+ 	else
+ 		return (true);
+ }
+ 
+ bool check_right_word_branches(t_tree_node *node, t_tree_node *node_direction)
+ {
+ 	if (node == NULL)
+ 		return (true);
+ 	if (node->token == WORD && node_direction != NULL)
+ 		return (false);
+ 	else
+ 		return (true);
+ }
+ 
+ t_tree_node *insert(t_tree_node *node, char *data, e_tokens token, bool *flag_inserted_node)
+ {
+     if (node == NULL){
+         return (create_tree_node(data, token, flag_inserted_node));}
+     
+ 	if (*flag_inserted_node == false
+ 		&& check_pipe_precendence(token, node->right)
+ 		&& check_builtin_cmd(node->right)
+ 		&& check_right_word_branches(node, node->left))
+ 	{
+ 		node->right = insert(node->right, data, token, flag_inserted_node);
+ 	}
+ 	if (*flag_inserted_node == false
+ 		&& check_pipe_precendence(token, node->left)
+ 		&& check_builtin_cmd(node->left)
+ 		&& check_right_word_branches(node, node->right)
+ 	)
+ 	{
+ 		node->left = insert(node->left, data, token, flag_inserted_node);
+ 	}
+     return (node);
+ }
+ 
+ t_tree	*stack_to_tree(t_stack *stack)
+ {
+ 	t_tree	*tree;
+ 	bool	flag_inserted_node;
+ 
+ 	tree = init_tree();
+ 	flag_inserted_node = false;
+ 	tree->root = create_tree_node(stack->stack[stack->top].data,
+ 			stack->stack[stack->top].token, &flag_inserted_node);
+ 	stack->top--;
+ 	while (stack->top > -1)
+ 	{
+ 		flag_inserted_node = false;
+ 		tree->root = insert(tree->root, stack->stack[stack->top].data,
+ 				stack->stack[stack->top].token, &flag_inserted_node);
+ 		stack->top--;
+ 	}
+ 	return (tree);
+ }
