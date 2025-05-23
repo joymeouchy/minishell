7ed3478a4b915d2d6c44e15c299464fb6f00d60d
Joy Meouchy <joymeouchy@gmail.com>
2025-05-23 11:20:18 +0300

    pipe not piping

--- src/execution/pipe.c
+++ src/execution/pipe.c
@@ -6,132 +6,62 @@
 /*   By: jmeouchy <jmeouchy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 18:51:42 by jmeouchy          #+#    #+#             */
-/*   Updated: 2025/05/17 16:22:17 by jmeouchy         ###   ########.fr       */
+/*   Updated: 2025/05/23 11:06:02 by jmeouchy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-// char *preserve_fds()
-// {
-//     char *fd
-// }
-
 void left_node(t_tree_node *node, int pipefd[2])
 {
-	// write(1, "i am in right node\n", 20);
-	dup2(pipefd[0], 0);  // Read from pipe
+	dup2(pipefd[0], 0);  // stdin = read end
 	close(pipefd[0]);
 	close(pipefd[1]);
-	execution(node->left); // Execute right node command
-	exit(0); // Important: exit after execution
+	execution(node->left);
+	exit(0);
 }
 
 void right_node(t_tree_node *node, int pipefd[2])
 {
-	// write(1, "i am in right node\n", 19);
-	dup2(pipefd[1], 1);  // Write to pipe
+	dup2(pipefd[1], 1);  // stdout = write end
 	close(pipefd[0]);
 	close(pipefd[1]);
-	execution(node->right); // Execute left node command
-	exit(0); // Important: exit after execution
+	execution(node->right);
+	exit(0);
 }
-
-
-
-// void pipe_exec(t_tree_node *node)
-// {
-// 	int pipefd[2];
-// 	pid_t pid;
-// 	int status;
-
-// 	if (pipe(pipefd) == -1)
-// 		return ;
-
-// 	pid = fork();
-// 	if (pid == -1)
-// 		return ;
-// 	if (pid == 0)
-// 		left_node(node, pipefd);
-// 	right_node(node, pipefd);
-// 		waitpid(pid, &status, 0);
-// 	// Parent: close pipe
-// 	// close(pipefd[0]);
-// 	// close(pipefd[1]);
-// }
-
-// void pipe_exec(t_tree_node *node)
-// {
-// 	int pipefd[2];
-// 	pid_t left_pid, right_pid;
-// 	int status;
-
-// 	if (pipe(pipefd) == -1)
-// 		return ;
-
-// 	left_pid = fork();
-// 	if (left_pid == -1)
-// 		return ;
-// 	if (left_pid == 0)
-// 		left_node(node, pipefd);
-
-// 	right_pid = fork();
-// 	if (right_pid == -1)
-// 		return ;
-// 	if (right_pid == 0)
-// 		right_node(node, pipefd);
-	
-// 	// Parent: close pipe
-// 	close(pipefd[0]);
-// 	close(pipefd[1]);
-
-// 	waitpid(left_pid, &status, 0);
-// 	waitpid(right_pid, &status, 0);
-
-	
-// }
-
-
 void pipe_exec(t_tree_node *node)
 {
-    int pipefd[2];
-    pid_t left_pid, right_pid;
+    int     pipefd[2];
+    pid_t   right_pid;
+    pid_t   left_pid;
 
     if (pipe(pipefd) == -1)
-        return ;
-
-    left_pid = fork();
-    if (left_pid == -1)
-        return ;
-    if (left_pid == 0)
-    {
-        // Left: write to pipe
-        dup2(pipefd[1], STDOUT_FILENO);
-        close(pipefd[0]);
-        close(pipefd[1]);
-
-        execution(node->right); // Can recurse into another pipe or command
-        exit(0);
-    }
-
+        return;
     right_pid = fork();
     if (right_pid == -1)
-        return ;
+        return;
     if (right_pid == 0)
     {
-        // Right: read from pipe
-        dup2(pipefd[0], STDIN_FILENO);
-        close(pipefd[1]);
-        close(pipefd[0]);
-
-        execution(node->left); // Can recurse into another pipe or command
-        exit(0);
+        if (node->right->token == PIPE || node->left->token == PIPE)
+            left_node(node->left, pipefd);
+        else
+            right_node(node, pipefd);
+        exit(1); // Should not reach
     }
-
-    // Parent: close pipe ends
+    left_pid = fork();
+    if (left_pid == -1)
+        return;
+    if (left_pid == 0)
+    {
+       if (node->right->token == PIPE || node->left->token == PIPE)
+            right_node(node->right, pipefd);
+        else
+            left_node(node, pipefd);
+        exit(1); // Should not reach
+    }
     close(pipefd[0]);
     close(pipefd[1]);
 
-    waitpid(left_pid, NULL, 0);
     waitpid(right_pid, NULL, 0);
+    waitpid(left_pid, NULL, 0);
 }
